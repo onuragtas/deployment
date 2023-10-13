10338867fe0874f36dcbb28c4fe96dbf963e8c7e
onuragtas <agtasonur@gmail.com>
2023-10-14 00:59:59 +0300

    config changed

---
+++ config.go
@@ -0,0 +1,20 @@
+package main
+
+var config Config
+
+type Config struct {
+	Username string    `yaml:"username"`
+	Token    string    `yaml:"token"`
+	Settings Settings  `yaml:"settings"`
+	Projects []Project `yaml:"projects"`
+}
+type Project struct {
+	Url    string `yaml:"url"`
+	Path   string `yaml:"path"`
+	Branch string `yaml:"branch"`
+	Script string `yaml:"script"`
+}
+
+type Settings struct {
+	CheckTime int `yaml:"check_time"`
+}
